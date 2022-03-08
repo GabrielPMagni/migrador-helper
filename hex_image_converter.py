982546f1c0b9fe3f6ce651d59221e6ee3bc67432
Gabriel Peres Magni <gpmagni@protonmail.com>
2022-03-08 05:36:02 -0300

    add logs

--- hex_image_converter.py
+++ hex_image_converter.py
@@ -1,6 +1,6 @@
 import argparse
 import re as regex
-from os import remove as rm, listdir as ls, path
+from os import path, mkdir
 import codecs
 
 
@@ -11,27 +11,56 @@ class HexImageConverter:
         self.output_folder = self.validate_output_folder_config(output_folder)
         self.codification = codification
         self.delimiter = delimiter
+        self.success_log_file, self.error_log_file = self.validate_log_files()
         self.get_images_with_id()
 
 
     def get_images_with_id(self):
         for index, line in enumerate(self.input_file.readlines()):
             if index == 0: continue
-            if index == 3: break
             identifier = regex.match(fr'^(.+?){self.delimiter}', line).group(1)
             file_extension = 'jpeg'
             hex_image = regex.search(r'0x(.+)$', line).group(1)
-            image_data = bytes.fromhex(hex_image)
-            with open(path.join(self.output_folder, identifier+'.'+file_extension), 'wb') as output_file:
-                output_file.write(image_data)
+            image_file_path = path.join(self.output_folder, identifier+'.'+file_extension)
+            try:
+                image_data = bytes.fromhex(hex_image)
+                with open(image_file_path, 'wb') as output_file:
+                    output_file.write(image_data)
+                self.log_to_success_file(image_file_path)
+            except Exception as e:
+                self.log_to_error_file(image_file_path + '\t' + str(e))
+            
         self.input_file.close()
 
+
+    def log_to_error_file(self, log):
+        self.error_log_file.write(log+'\n')
+
+
+    def log_to_success_file(self, log):
+        self.success_log_file.write(log+'\n')
 
 
     def confirm(self, question='') -> bool:
         answer = input(question+'\n\n\t> [S/n]').lower()
         return True if (answer or answer in ['yes', 'sim', 's', 'y']) else False
-            
+
+
+    def validate_log_files(self):
+        try:
+            logs_path = 'logs'
+            mkdir(logs_path)
+            success_log = codecs.open(path.join(logs_path, 'events.log'), 'a+', encoding=self.codification, errors='strict')
+            error_log = codecs.open(path.join(logs_path, 'errors.log'), 'a+', encoding=self.codification, errors='strict')
+            return {success_log, error_log}
+        except PermissionError:
+            print('Sem permissão para criar arquivos de log.')
+            exit(1)
+        except Exception as e:
+            print(f'Erro não tratado ao tentar abrir criar arquivos de log: {str(e)}')
+            exit(1)
+
+
 
     def validate_output_folder_config(self, output_folder):
         try:
